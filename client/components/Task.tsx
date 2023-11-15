ed82ec8ca28bed3fca8b5dd8b4fadf0c42afe5aa
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-16 11:11:57 +1300

    Complete function to delete task

--- client/components/Task.tsx
+++ client/components/Task.tsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react'
 import * as models from '../models/TasksModel'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteTaskApi } from '../apis/tasksapi'
+// import e from 'express'
 
 interface Props {
   tasks: any
 }
 
 function Task(prop: Props) {
-  const [deleteButton, setDeleteButton] = useState('')
+  // const [deleteButton, setDeleteButton] = useState('')
+  const mutateDeleteTask = useMutation({
+    mutationFn: (id: number) => deleteTaskApi(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['tasks'])
+    },
+  })
+
+  const queryClient = useQueryClient()
 
   const allTasks = prop.tasks
 
-  function handleClick(id) {
-    // e.preventDefault()
-    setDeleteButton(id)
-    console.log(deleteButton)
+  async function handleClick(
+    e: React.MouseEvent<HTMLButtonElement>,
+    id: number
+  ) {
+    e.preventDefault()
+    // console.log(e)
+    mutateDeleteTask.mutate(id)
+    console.log(id)
+    // setDeleteButton(id)
+    // console.log(deleteButton)
   }
 
   return allTasks.map((task: models.Task) => (
-    <li key={allTasks.id}>
+    <li key={task.id}>
       <div className="view">
         <input className="toggle" type="checkbox" />
         <label>{task.task}</label>
         <button
-          onClick={() => handleClick(allTasks.id)}
+          onClick={(e) => handleClick(e, task.id)}
           className="destroy"
-          key={allTasks.id}
         ></button>
       </div>
       <input className="edit" value="Rule the web" />
