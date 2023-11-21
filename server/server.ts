5d2f354578b124297b23b39ed2c39a2a4c387c56
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-22 10:00:48 +1300

    Attempt 2

--- server/server.ts
+++ server/server.ts
@@ -7,11 +7,26 @@ const server = express()
 server.use(express.json())
 server.use('/api/v1/todos', tasks)
 
+// if (process.env.NODE_ENV === 'production') {
+//   server.use(express.static(Path.resolve('public')))
+//   server.use('/assets', express.static(Path.resolve('./dist/assets')))
+//   server.get('*', (req, res) => {
+//     res.sendFile(Path.resolve('./dist/index.html'))
+//   })
+// }
+
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
+
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static('../assets'))
+  server.get('*', (req, res) => {
+    res.sendFile('../index.html')
   })
 }
 
