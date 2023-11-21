d95b2bb7cf592555fda0c75fd5d77e9ec6e76328
Samir Lekinwala <samirlekinwala@gmail.com>
2023-11-22 09:56:09 +1300

    Attempt to fix deployment

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ server.use(express.json())
 server.use('/api/v1/todos', tasks)
 
 if (process.env.NODE_ENV === 'production') {
-  // server.use(express.static(Path.resolve('public')))
+  server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
